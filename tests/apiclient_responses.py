10ca824d98fed54eca083ff5d3e13370a547fa37
Mark Troyer <mtroyer@zuora.com>
2018-08-13 13:54:31 -0700

    Tests for API client module

---
+++ tests/apiclient_responses.py
@@ -0,0 +1,525 @@
+auth_error = {
+    u'error_id': u'auth.invalid',
+    u'error_text': u'User name or password was invalid'
+}
+
+config_endpoints = {
+    u'children': [
+        {
+            u'href': u'/api/tm/5.2/config/active/action_programs/',
+            u'name': u'action_programs'
+        },
+        {
+            u'href': u'/api/tm/5.2/config/active/actions/',
+            u'name': u'actions'
+        },
+        {
+            u'href': u'/api/tm/5.2/config/active/appliance/',
+            u'name': u'appliance'
+        },
+        {
+            u'href': u'/api/tm/5.2/config/active/application_firewall',
+            u'name': u'application_firewall'
+        },
+        {
+            u'href': u'/api/tm/5.2/config/active/aptimizer/',
+            u'name': u'aptimizer'
+        },
+        {
+            u'href': u'/api/tm/5.2/config/active/bandwidth/',
+            u'name': u'bandwidth'
+        },
+        {
+            u'href': u'/api/tm/5.2/config/active/bgpneighbors/',
+            u'name': u'bgpneighbors'
+        },
+        {
+            u'href': u'/api/tm/5.2/config/active/cloud_api_credentials/',
+            u'name': u'cloud_api_credentials'
+        },
+        {
+            u'href': u'/api/tm/5.2/config/active/custom/',
+            u'name': u'custom'
+        },
+        {
+            u'href': u'/api/tm/5.2/config/active/dns_server/',
+            u'name': u'dns_server'
+        },
+        {
+            u'href': u'/api/tm/5.2/config/active/event_types/',
+            u'name': u'event_types'
+        },
+        {
+            u'href': u'/api/tm/5.2/config/active/extra_files/',
+            u'name': u'extra_files'
+        },
+        {
+            u'href': u'/api/tm/5.2/config/active/glb_services/',
+            u'name': u'glb_services'
+        },
+        {
+            u'href': u'/api/tm/5.2/config/active/global_settings',
+            u'name': u'global_settings'
+        },
+        {
+            u'href': u'/api/tm/5.2/config/active/kerberos/',
+            u'name': u'kerberos'
+        },
+        {
+            u'href': u'/api/tm/5.2/config/active/license_keys/',
+            u'name': u'license_keys'
+        },
+        {
+            u'href': u'/api/tm/5.2/config/active/locations/',
+            u'name': u'locations'
+        },
+        {
+            u'href': u'/api/tm/5.2/config/active/log_export/',
+            u'name': u'log_export'
+        },
+        {
+            u'href': u'/api/tm/5.2/config/active/monitor_scripts/',
+            u'name': u'monitor_scripts'
+        },
+        {
+            u'href': u'/api/tm/5.2/config/active/monitors/',
+            u'name': u'monitors'
+        },
+        {
+            u'href': u'/api/tm/5.2/config/active/persistence/',
+            u'name': u'persistence'
+        },
+        {
+            u'href': u'/api/tm/5.2/config/active/pools/',
+            u'name': u'pools'
+        },
+        {
+            u'href': u'/api/tm/5.2/config/active/protection/',
+            u'name': u'protection'
+        },
+        {
+            u'href': u'/api/tm/5.2/config/active/rate/',
+            u'name': u'rate'
+        },
+        {
+            u'href': u'/api/tm/5.2/config/active/rule_authenticators/',
+            u'name': u'rule_authenticators'
+        },
+        {
+            u'href': u'/api/tm/5.2/config/active/rules/',
+            u'name': u'rules'
+        },
+        {
+            u'href': u'/api/tm/5.2/config/active/saml/',
+            u'name': u'saml'
+        },
+        {
+            u'href': u'/api/tm/5.2/config/active/security',
+            u'name': u'security'
+        },
+        {
+            u'href': u'/api/tm/5.2/config/active/service_level_monitors/',
+            u'name': u'service_level_monitors'
+        },
+        {
+            u'href': u'/api/tm/5.2/config/active/servicediscovery/',
+            u'name': u'servicediscovery'
+        },
+        {
+            u'href': u'/api/tm/5.2/config/active/ssl/',
+            u'name': u'ssl'
+        },
+        {
+            u'href': u'/api/tm/5.2/config/active/traffic_ip_groups/',
+            u'name': u'traffic_ip_groups'
+        },
+        {
+            u'href': u'/api/tm/5.2/config/active/traffic_managers/',
+            u'name': u'traffic_managers'
+        },
+        {
+            u'href': u'/api/tm/5.2/config/active/user_authenticators/',
+            u'name': u'user_authenticators'
+        },
+        {
+            u'href': u'/api/tm/5.2/config/active/user_groups/',
+            u'name': u'user_groups'
+        },
+        {
+            u'href': u'/api/tm/5.2/config/active/virtual_servers/',
+            u'name': u'virtual_servers'
+        }
+    ]
+}
+
+config_endpoints_v1 = {
+    u'children': [
+        {
+            u'name': u'actionprogs',
+            u'href': u'/api/tm/1.0/config/active/actionprogs/'
+        },
+        {
+            u'name': u'actions',
+            u'href': u'/api/tm/1.0/config/active/actions/'
+        },
+        {
+            u'name': u'aptimizer',
+            u'href': u'/api/tm/1.0/config/active/aptimizer/'
+        },
+        {
+            u'name': u'auth',
+            u'href': u'/api/tm/1.0/config/active/auth/'
+        },
+        {
+            u'name': u'authenticators',
+            u'href': u'/api/tm/1.0/config/active/authenticators/'
+        },
+        {
+            u'name': u'bandwidth',
+            u'href': u'/api/tm/1.0/config/active/bandwidth/'
+        },
+        {
+            u'name': u'cloudcredentials',
+            u'href': u'/api/tm/1.0/config/active/cloudcredentials/'
+        },
+        {
+            u'name': u'events',
+            u'href': u'/api/tm/1.0/config/active/events/'
+        },
+        {
+            u'name': u'extra',
+            u'href': u'/api/tm/1.0/config/active/extra/'
+        },
+        {
+            u'name': u'flipper',
+            u'href': u'/api/tm/1.0/config/active/flipper/'
+        },
+        {
+            u'name': u'groups',
+            u'href': u'/api/tm/1.0/config/active/groups/'
+        },
+        {
+            u'name': u'licensekeys',
+            u'href': u'/api/tm/1.0/config/active/licensekeys/'
+        },
+        {
+            u'name': u'locations',
+            u'href': u'/api/tm/1.0/config/active/locations/'
+        },
+        {
+            u'name': u'monitors',
+            u'href': u'/api/tm/1.0/config/active/monitors/'
+        },
+        {
+            u'name': u'persistence',
+            u'href': u'/api/tm/1.0/config/active/persistence/'
+        },
+        {
+            u'name': u'pools',
+            u'href': u'/api/tm/1.0/config/active/pools/'
+        },
+        {
+            u'name': u'protection',
+            u'href': u'/api/tm/1.0/config/active/protection/'
+        },
+        {
+            u'name': u'rate',
+            u'href': u'/api/tm/1.0/config/active/rate/'
+        },
+        {
+            u'name': u'rules',
+            u'href': u'/api/tm/1.0/config/active/rules/'
+        },
+        {
+            u'name': u'scripts',
+            u'href': u'/api/tm/1.0/config/active/scripts/'
+        },
+        {
+            u'name': u'security',
+            u'href': u'/api/tm/1.0/config/active/security'
+        },
+        {
+            u'name': u'services',
+            u'href': u'/api/tm/1.0/config/active/services/'
+        },
+        {
+            u'name': u'settings.cfg',
+            u'href': u'/api/tm/1.0/config/active/settings.cfg'
+        },
+        {
+            u'name': u'slm',
+            u'href': u'/api/tm/1.0/config/active/slm/'
+        },
+        {
+            u'name': u'ssl',
+            u'href': u'/api/tm/1.0/config/active/ssl/'
+        },
+        {
+            u'name': u'vservers',
+            u'href': u'/api/tm/1.0/config/active/vservers/'
+        },
+        {
+            u'name': u'zxtms',
+            u'href': u'/api/tm/1.0/config/active/zxtms/'
+        }
+    ]
+}
+
+full_backups = {
+    u'children': [
+        {
+            u'name': u'Backup1',
+            u'href': u'/api/tm/5.2/status/local_tm/backups/full/Backup1'
+        }
+    ]
+}
+
+get_backup = {
+    u'properties': {
+        u'backup': {
+            u'description': u'Test Backup',
+            u'time_stamp': 1530257980,
+            u'version': u'18.1',
+        }
+    }
+}
+
+status_info = {
+    u'information': {
+        u'tm_version': u'18.1',
+        u'uuid': u'c8cd17b3-f23e-3601-84e9-0a71387cf724'
+    }
+}
+
+statistics = {
+    u'children': [
+        {
+            u'name': u'actions',
+            u'href': u'/api/tm/5.2/status/local_tm/statistics/actions/'
+        },
+        {
+            u'href': u'/api/tm/5.2/status/local_tm/statistics/bandwidth/',
+            u'name': u'bandwidth'
+        },
+        {
+            u'href': u'/api/tm/5.2/status/local_tm/statistics/cache/',
+            u'name': u'cache'
+        },
+        {
+            u'href': u'/api/tm/5.2/status/local_tm/statistics/cloud_api_credentials/',
+            u'name': u'cloud_api_credentials'
+        },
+        {
+            u'href': u'/api/tm/5.2/status/local_tm/statistics/connection_rate_limit/',
+            u'name': u'connection_rate_limit'
+        },
+        {
+            u'href': u'/api/tm/5.2/status/local_tm/statistics/events/',
+            u'name': u'events'
+        },
+        {
+            u'href': u'/api/tm/5.2/status/local_tm/statistics/extras/',
+            u'name': u'extras'
+        },
+        {
+            u'href': u'/api/tm/5.2/status/local_tm/statistics/glb_services/',
+            u'name': u'glb_services'
+        },
+        {
+            u'href': u'/api/tm/5.2/status/local_tm/statistics/globals',
+            u'name': u'globals'
+        },
+        {
+            u'href': u'/api/tm/5.2/status/local_tm/statistics/listen_ips/',
+            u'name': u'listen_ips'
+        },
+        {
+            u'href': u'/api/tm/5.2/status/local_tm/statistics/locations/',
+            u'name': u'locations'
+        },
+        {
+            u'href': u'/api/tm/5.2/status/local_tm/statistics/network_interface/',
+             u'name': u'network_interface'
+        },
+        {
+            u'href': u'/api/tm/5.2/status/local_tm/statistics/nodes/',
+            u'name': u'nodes'
+        },
+        {
+            u'href': u'/api/tm/5.2/status/local_tm/statistics/per_location_service/',
+            u'name': u'per_location_service'
+        },
+        {
+            u'href': u'/api/tm/5.2/status/local_tm/statistics/per_node_slm/',
+            u'name': u'per_node_slm'
+        },
+        {
+            u'href': u'/api/tm/5.2/status/local_tm/statistics/pools/',
+            u'name': u'pools'
+        },
+        {
+            u'href': u'/api/tm/5.2/status/local_tm/statistics/rule_authenticators/',
+            u'name': u'rule_authenticators'
+        },
+        {
+            u'href': u'/api/tm/5.2/status/local_tm/statistics/rules/',
+            u'name': u'rules'
+        },
+        {
+            u'href': u'/api/tm/5.2/status/local_tm/statistics/service_level_monitors/',
+            u'name': u'service_level_monitors'
+        },
+        {
+            u'href': u'/api/tm/5.2/status/local_tm/statistics/service_protection/',
+            u'name': u'service_protection'
+        },
+        {
+            u'href': u'/api/tm/5.2/status/local_tm/statistics/ssl_ocsp_stapling',
+            u'name': u'ssl_ocsp_stapling'
+        },
+        {
+            u'href': u'/api/tm/5.2/status/local_tm/statistics/traffic_ips/',
+            u'name': u'traffic_ips'
+        },
+        {
+            u'href': u'/api/tm/5.2/status/local_tm/statistics/virtual_servers/',
+            u'name': u'virtual_servers'
+        }
+    ]
+}
+
+statistics_globals = {
+    u'statistics': {
+        u'analytics_transactions_dropped': 0,
+        u'analytics_transactions_exported': 0,
+        u'analytics_transactions_memory_usage': 0,
+        u'data_entries': 0,
+        u'data_memory_usage': 16861912,
+        u'events_seen': 184658,
+        u'hourly_peak_bytes_in_per_second': 333264,
+        u'hourly_peak_bytes_out_per_second': 11903631,
+        u'hourly_peak_requests_per_second': 128,
+        u'hourly_peak_ssl_connections_per_second': 63,
+        u'num_idle_connections': 1,
+        u'number_child_processes': 8,
+        u'number_dnsa_cache_hits': 0,
+        u'number_dnsa_requests': 0,
+        u'number_dnsptr_cache_hits': 0,
+        u'number_dnsptr_requests': 0,
+        u'number_snmp_bad_requests': 0,
+        u'number_snmp_get_bulk_requests': 0,
+        u'number_snmp_get_next_requests': 203357,
+        u'number_snmp_get_requests': 2125772,
+        u'number_snmp_unauthorised_requests': 0,
+        u'ssl_cipher_3des_decrypts': 118512,
+        u'ssl_cipher_3des_encrypts': 137376,
+        u'ssl_cipher_aes_decrypts': 1857008912,
+        u'ssl_cipher_aes_encrypts': 2674129360,
+        u'ssl_cipher_aes_gcm_decrypts': 1066250097,
+        u'ssl_cipher_aes_gcm_encrypts': 793920689,
+        u'ssl_cipher_decrypts': 2923377521,
+        u'ssl_cipher_des_decrypts': 0,
+        u'ssl_cipher_des_encrypts': 0,
+        u'ssl_cipher_dh_agreements': 106707,
+        u'ssl_cipher_dh_generates': 168128,
+        u'ssl_cipher_dsa_signs': 0,
+        u'ssl_cipher_dsa_verifies': 0,
+        u'ssl_cipher_ecdh_agreements': 58378,
+        u'ssl_cipher_ecdh_generates': 68913,
+        u'ssl_cipher_ecdsa_signs': 0,
+        u'ssl_cipher_ecdsa_verifies': 0,
+        u'ssl_cipher_encrypts': 3468187425,
+        u'ssl_cipher_rc4_decrypts': 0,
+        u'ssl_cipher_rc4_encrypts': 0,
+        u'ssl_cipher_rsa_decrypts': 45926258,
+        u'ssl_cipher_rsa_decrypts_external': 0,
+        u'ssl_cipher_rsa_encrypts': 237041,
+        u'ssl_cipher_rsa_encrypts_external': 0,
+        u'ssl_client_cert_expired': 0,
+        u'ssl_client_cert_invalid': 0,
+        u'ssl_client_cert_not_sent': 0,
+        u'ssl_client_cert_revoked': 0,
+        u'ssl_connections': 98340362,
+        u'ssl_handshake_sslv3': 0,
+        u'ssl_handshake_tlsv1': 226643,
+        u'ssl_handshake_tlsv11': 246797,
+        u'ssl_handshake_tlsv12': 99449636,
+        u'ssl_session_id_mem_cache_hit': 44833597,
+        u'ssl_session_id_mem_cache_miss': 298057,
+        u'sys_cpu_busy_percent': 3,
+        u'sys_cpu_idle_percent': 96,
+        u'sys_cpu_system_busy_percent': 0,
+        u'sys_cpu_user_busy_percent': 2,
+        u'sys_fds_free': 8385733,
+        u'sys_mem_buffered': 262,
+        u'sys_mem_free': 30268,
+        u'sys_mem_in_use': 1909,
+        u'sys_mem_swap_total': 859,
+        u'sys_mem_swapped': 0,
+        u'sys_mem_total': 32177,
+        u'time_last_config_update': 15594000,
+        u'total_backend_server_errors': 0,
+        u'total_bad_dns_packets': 0,
+        u'total_bytes_in': 590699663658,
+        u'total_bytes_out': 5387886961402,
+        u'total_conn': 100652767,
+        u'total_current_conn': 11,
+        u'total_dns_responses': 0,
+        u'total_requests': 513259483,
+        u'total_transactions': 0,
+        u'up_time': 690634000
+    }
+}
+
+cache_stats = {
+    u'children': [
+        {
+            u'href': u'/api/tm/5.2/status/local_tm/statistics/cache/asp_session_cache',
+            u'name': u'asp_session_cache'
+        },
+        {
+            u'href': u'/api/tm/5.2/status/local_tm/statistics/cache/ip_session_cache',
+            u'name': u'ip_session_cache'
+        },
+        {
+            u'href': u'/api/tm/5.2/status/local_tm/statistics/cache/j2ee_session_cache',
+            u'name': u'j2ee_session_cache'
+        },
+        {
+            u'href': u'/api/tm/5.2/status/local_tm/statistics/cache/ssl_cache',
+            u'name': u'ssl_cache'
+        },
+        {
+            u'href': u'/api/tm/5.2/status/local_tm/statistics/cache/ssl_session_cache',
+            u'name': u'ssl_session_cache'
+        },
+        {
+            u'href': u'/api/tm/5.2/status/local_tm/statistics/cache/uni_session_cache',
+            u'name': u'uni_session_cache'
+        },
+        {
+            u'href': u'/api/tm/5.2/status/local_tm/statistics/cache/web_cache',
+            u'name': u'web_cache'
+        }
+    ]
+}
+
+web_cache = {
+    u'statistics': {
+        u'entries': 0,
+        u'hit_rate': 0,
+        u'hits': 0,
+        u'lookups': 0,
+        u'max_entries': 10000,
+        u'mem_maximum': 6589440,
+        u'mem_used': 0,
+        u'misses': 0,
+        u'oldest': 0,
+        u'url_store_allocated': 600,
+        u'url_store_free': 5360512,
+        u'url_store_size': 5361112,
+        u'url_store_total_allocations': 0,
+        u'url_store_total_failures': 0,
+        u'url_store_total_frees': 0
+    }
+}
