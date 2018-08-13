8a15847418e06530d13872ed28a514c8687b00a3
Mark Troyer <mtroyer@zuora.com>
2018-08-13 13:54:07 -0700

    API client module

---
+++ src/stingray/__init__.py
@@ -0,0 +1,9 @@
+"""
+Stingray/Zeus/Pulse Secure load balancer API module
+"""
+
+__major__ = '0'
+__minor__ = '1'
+__update__ = '0'
+__version__ = '{0}.{1}'.format(__major__, __minor__)
+__release__ = '{0}.{1}'.format(__version__, __update__)
